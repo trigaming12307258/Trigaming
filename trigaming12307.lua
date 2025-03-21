e350e6c902eba1fada753fdc7f46fdeea018cbd0
trigaming12307258 <tri12307net@gmail.com>
2025-03-21 14:47:26 +0700

    Add files via upload

---
+++ trigaming12307.lua
@@ -0,0 +1,67 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "trigaming",
+         Animation = "haha mấy đồ ko biết làm script "
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=83190276951914",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script Farm"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Redz Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+  end
+  }) 
+   
+    AddButton(Tab1o, {
+     Name = "hiruhud ",
+    Callback = function()
+	  local Settings = { 
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/NGUYENVUDUY1/Dev-Hiru/refs/heads/main/HiruHub.lua"))() 
+	 AddButton(Tab1o, {
+     Name = "akautochest",
+    Callback = function()
+	  local Settings = { 
+	  getgenv().Team = "Marines" -- Pirates/Marines
+
+getgenv().Hide_Menu = false -- true/false
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditoiletgojo/Haidepzai/refs/heads/main/Autochest-Akgamingez"))() 
+AddButton(Tab1o, {
+     Name = "mingaming Hub",
+    Callback = function()
+	  local Settings = {
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinMV"))()
